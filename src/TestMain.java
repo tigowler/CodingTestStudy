8a921f35a887aa4228ce83f9737e14c1b46c9936
tigowler <aisapple1030@naver.com>
2022-02-10 17:52:36 +0900

    create mergesort

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,30 +1,14 @@
 import dataStructure.BinaryTree;
 import sort.BubbleSort;
 import sort.InsertionSort;
+import sort.MergeSort;
 import sort.SelectionSort;
 
 import java.util.ArrayList;
 
 public class TestMain {
     public static void main(String[] args) {
-//        dataStructure.SingleLinkedList<Integer> MyLinkedList = new dataStructure.SingleLinkedList<Integer>();
-//        MyLinkedList.test(MyLinkedList);
 
-//        dataStructure.DoubleLinkedList<Integer> dll = new dataStructure.DoubleLinkedList<>();
-//        dll.test(dll);
-
-//        dataStructure.HashTableWithChaining htc = new dataStructure.HashTableWithChaining(20);
-//        htc.test(htc);
-
-//        dataStructure.HashTableWithLinearProbing htlp = new dataStructure.HashTableWithLinearProbing(20);
-//        htlp.test(htlp);
-
-//        BinaryTree tree = new BinaryTree();
-//        tree.test(tree);
-
-//        bubbleSortTest();
-//        selectionSortTest();
-        insertionSortTest();
     }
 
 
@@ -36,18 +20,4 @@ public class TestMain {
         return testData;
     }
 
-    public static void bubbleSortTest(){
-        BubbleSort bubbleSort = new BubbleSort();
-        System.out.println(bubbleSort.sort(createRandomArray()));
-    }
-
-    public static void selectionSortTest(){
-        SelectionSort selectionSort = new SelectionSort();
-        System.out.println(selectionSort.sort(createRandomArray()));
-    }
-
-    public static void insertionSortTest(){
-        InsertionSort insertionSort = new InsertionSort();
-        System.out.println(InsertionSort.sort(createRandomArray()));
-    }
 }
---
+++ src/sort/MergeSort.java
@@ -0,0 +1,48 @@
+package sort;
+
+import java.util.ArrayList;
+
+public class MergeSort {
+    public ArrayList<Integer> mergeFunc(ArrayList<Integer> leftList, ArrayList<Integer> rightList){
+        ArrayList<Integer> mergedList = new ArrayList<Integer>();
+        Integer leftPoint = 0;
+        Integer rightPoint = 0;
+
+        // 1) left/right 둘 다 존재할 때
+        while(leftList.size()>leftPoint && rightList.size()>rightPoint){
+            if(leftList.get(leftPoint)>rightList.get(rightPoint)){
+                mergedList.add(rightList.get(rightPoint));
+                rightPoint+=1;
+            } else {
+                mergedList.add(leftList.get(leftPoint));
+                leftPoint += 1;
+            }
+        }
+
+        // 2) left 데이터만 있을 때
+        while(leftList.size()>leftPoint){
+            mergedList.add(leftList.get(leftPoint));
+            leftPoint+=1;
+        }
+
+        // 3) right 데이터만 있을 때
+        while(rightList.size()>rightPoint){
+            mergedList.add(rightList.get(rightPoint));
+            rightPoint+=1;
+        }
+
+        return mergedList;
+    }
+
+    public ArrayList<Integer> mergeSplitFunc(ArrayList<Integer> dataList){
+        if (dataList.size()<=1){
+            return dataList;
+        }
+        int medium = dataList.size()/2;
+
+        ArrayList<Integer> leftArr = this.mergeSplitFunc(new ArrayList<Integer>(dataList.subList(0, medium)));
+        ArrayList<Integer> rightArr = this.mergeSplitFunc(new ArrayList<Integer>(dataList.subList(medium, dataList.size())));
+
+        return this.mergeFunc(leftArr, rightArr);
+    }
+}
