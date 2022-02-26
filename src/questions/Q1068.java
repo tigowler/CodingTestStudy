26f8f67da0519ccf915ce9daa8d31989827d2aa5
tigowler <aisapple1030@naver.com>
2022-02-26 15:29:30 +0900

    Tree

---
+++ src/questions/Q1068.java
@@ -0,0 +1,104 @@
+package questions;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Q1068 {
+    static FastReader in = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static int n, root, erased;
+    static ArrayList<Integer>[] child;
+    //leaf[x] := x를 root로 하는 subtree에 있는 단말 노드의 개수
+    //x가 단말 노드인 경우 => leaf[x] = 1
+    //아닌 경우 => x의 자식들에 대해 leaf를 먼저 계산한 후 leaf[x] = leaf[x의 자식 노드들]의 합
+    static int[] leaf;
+
+    static void input(){
+        n = in.nextInt();
+        child = new ArrayList[n];
+        leaf = new int[n];
+        for (int i=0; i<n; i++) child[i] = new ArrayList<>();
+        for (int i=0; i<n; i++){
+            int par = in.nextInt();
+            if (par == -1){
+                root = i;
+                continue;
+            }
+            child[par].add(i);
+        }
+        erased = in.nextInt();
+    }
+
+    static void dfs(int x){
+        if (child[x].isEmpty()){
+            leaf[x] = 1;
+        }
+        for (int y:  child[x]){
+            dfs(y);
+            //leaf[y]가 계산된 상태로 돌아옴
+            leaf[x] += leaf[y];
+        }
+    }
+
+    static void pro(){
+        for (int i=0; i<n; i++){
+            if (child[i].contains(erased)){
+                child[i].remove(child[i].indexOf(erased));
+            }
+        }
+        if (root!=erased) dfs(root);
+        System.out.println(leaf[root]);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
---
+++ src/questions/Q11725.java
@@ -0,0 +1,93 @@
+package questions;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Q11725 {
+    static FastReader in = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static ArrayList<Integer>[] adj;
+    static int[] parent;
+
+    static void input(){
+        N = in.nextInt();
+        adj = new ArrayList[N+1];
+        parent = new int[N+1];
+        for (int i=1; i<=N; i++) adj[i] = new ArrayList<>();
+        for (int i=1; i<N; i++){
+            int x = in.nextInt(), y = in.nextInt();
+            adj[x].add(y);
+            adj[y].add(x);
+        }
+    }
+
+    static void dfs(int x, int par){
+        for (int y: adj[x]){
+            //양방향 그래프라 자신과 연결된 부모도 adj 배열 안에 있음. 부모는 자식이 아니므로 제외
+            if (y == par) continue;
+            parent[y] = x;
+            dfs(y, x);
+        }
+    }
+
+    static void pro(){
+        dfs(1, -1);
+        for (int i=2; i<=N; i++){
+            sb.append(parent[i]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
